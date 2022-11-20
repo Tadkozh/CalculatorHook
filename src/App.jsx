b7d28e2ab2fc06e4f66e794422ac72e2e6b5ca2c
Tadkozh <tad.kozh.bihan@free.fr>
2022-11-20 23:26:03 +0100

    application V1 : with classes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,94 @@
-import { useState } from 'react'
+import React from 'react'
+import calculate from './functions/calculate'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+class Display extends React.Component {
+  render() {
+    return (
+      <div className="component-display">
+        <div>{this.props.value}</div>
+      </div>
+    )
+  }
+}
+class Button extends React.Component {
+  handleClick = () => {
+    this.props.clickHandler(this.props.name)
+  }
 
-  return (
-    <div>
-      calculatriceios
-    </div>
-  )
+  render() {
+    const className = [
+      'component-button',
+      this.props.orange ? 'orange' : '',
+      this.props.wide ? 'wide' : '',
+    ]
+
+    return (
+      <div className={className.join(' ').trim()}>
+        <button onClick={this.handleClick}>{this.props.name}</button>
+      </div>
+    )
+  }
+}
+class ButtonPanel extends React.Component {
+  handleClick = buttonName => {
+    this.props.clickHandler(buttonName)
+  }
+
+  render() {
+    return (
+      <div className="component-button-panel">
+        <div>
+          <Button name="AC" clickHandler={this.handleClick} />
+          <Button name="+/-" clickHandler={this.handleClick} />
+          <Button name="%" clickHandler={this.handleClick} />
+          <Button name="÷" clickHandler={this.handleClick} orange />
+        </div>
+        <div>
+          <Button name="7" clickHandler={this.handleClick} />
+          <Button name="8" clickHandler={this.handleClick} />
+          <Button name="9" clickHandler={this.handleClick} />
+          <Button name="x" clickHandler={this.handleClick} orange />
+        </div>
+        <div>
+          <Button name="4" clickHandler={this.handleClick} />
+          <Button name="5" clickHandler={this.handleClick} />
+          <Button name="6" clickHandler={this.handleClick} />
+          <Button name="-" clickHandler={this.handleClick} orange />
+        </div>
+        <div>
+          <Button name="1" clickHandler={this.handleClick} />
+          <Button name="2" clickHandler={this.handleClick} />
+          <Button name="3" clickHandler={this.handleClick} />
+          <Button name="+" clickHandler={this.handleClick} orange />
+        </div>
+        <div>
+          <Button name="0" clickHandler={this.handleClick} wide />
+          <Button name="." clickHandler={this.handleClick} />
+          <Button name="=" clickHandler={this.handleClick} orange />
+        </div>
+      </div>
+    )
+  }
 }
 
-export default App
+export default class App extends React.Component {
+  state = {
+    total: null,
+    next: null,
+    operation: null,
+  }
+
+  handleClick = buttonName => {
+    this.setState(calculate(this.state, buttonName))
+  }
+
+  render() {
+    return (
+      <div className="component-app">
+        <Display value={this.state.next || this.state.total || '0'} />
+        <ButtonPanel clickHandler={this.handleClick} />
+      </div>
+    )
+  }
+}
